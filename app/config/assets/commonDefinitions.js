db61460b6ca3693e94bc030befc27ac4a0760361
Celso <celso@sapiensconsult.com>
2019-07-07 14:20:05 +0200

    celsobat

--- app/config/assets/commonDefinitions.js
+++ app/config/assets/commonDefinitions.js
@@ -10,6 +10,7 @@ window.odkCommonDefinitions = {
             "text": {
               "en": "English",
               "hi": "अंग्रेज़ी",
+              "es": "Inglés",
               "default": "English"
             }
           }
@@ -23,12 +24,27 @@ window.odkCommonDefinitions = {
             "text": {
               "en": "Hindi",
               "hi": "हिंदी",
+              "es": "Hindi",
               "default": "Hindi"
             }
           }
         },
         "_row_num": 8,
         "name": "hi"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "en": "Spanish",
+              "hi": "स्पेनिश",
+              "es": "Español",
+              "default": "Spanish"
+            }
+          }
+        },
+        "_row_num": 9,
+        "name": "es"
       }
     ]
   },
