5c62b4d59f1e83f515226a06abedf7a959a9e002
Emil Rossing <emil@lemur.dk>
2019-08-23 22:12:36 +0200

    Trial and error

--- app/config/assets/js/listPersons.js
+++ app/config/assets/js/listPersons.js
@@ -23,8 +23,7 @@ function listPersons(region, sector, tabanca) {
         console.log("Found " + result.getCount() + " persons");
         var persons = [];
         for (var row = 0; row < result.getCount(); row++) {
-            // var id = result.getData(row,"_id"); // or _row_etag
-            var id = result.getData(row,"_row_etag");
+            var id = result.getData(row,"_id"); // or _row_etag
             var id_paciente = result.getData(row,"id_paciente");
             var nome =  result.getData(row,"nome");
             var nome_casa =  result.getData(row,"nome_casa");
@@ -53,7 +52,7 @@ function listPersons(region, sector, tabanca) {
     var failureFn = function( errorMsg ) {
         console.error('Failed to get persons from database: ' + errorMsg);
     }
-    var sql = 'SELECT _row_etag, id_paciente, nome, nome_casa, sexo, dn, ano, mes, af FROM QPS WHERE region = "' + region + '" COLLATE NOCASE AND sector = "' + sector + '" COLLATE NOCASE AND tabanca = "' + tabanca + '" COLLATE NOCASE ORDER BY id_paciente';
+    var sql = 'SELECT _id, id_paciente, nome, nome_casa, sexo, dn, ano, mes, af FROM QPS WHERE region = "' + region + '" COLLATE NOCASE AND sector = "' + sector + '" COLLATE NOCASE AND tabanca = "' + tabanca + '" COLLATE NOCASE ORDER BY id_paciente';
     odkData.arbitraryQuery('QPS', sql, null, null, null, successFn, failureFn);
 }
 
@@ -83,7 +82,14 @@ function getAgeString(person) {
 
 function addPersonDiv(p) {    
     var nome = (p.nome_casa && p.nome_casa.length) ? nome = p.nome + " (" + p.nome_casa + ")" : p.nome;        
-    $('#theList tr:last').after("<tr><td>" + p.id_paciente + "</td><td>" + nome + "</td><td>" + getSexoString(p.sexo) + "</td><td>" + getAgeString(p) + "</td><td>" + p.af + "</td><td><input onclick='openPersonForm(\"" + p.id + "\")' type='button' value='&gt;&gt;'></input></td></tr>");
+    //$('#theList tr:last').after("<tr><td>" + p.id_paciente + "</td><td>" + nome + "</td><td>" + getSexoString(p.sexo) + "</td><td>" + getAgeString(p) + "</td><td>" + p.af + "</td><td><input onclick='openPersonForm(\"" + p.id + "\")' type='button' value='&gt;&gt;'></input></td></tr>");
+    var butt = "<input onclick='openPersonForm(\"" + p.id + "\")' type='button' value='&gt;&gt;'></input>";
+    butt = butt + "<input onclick='openPersonForm2(\"" + p.id + "\")' type='button' value='&gt;2&gt;'></input>";
+    butt = butt + "<input onclick='openPersonForm3(\"" + p.id + "\")' type='button' value='&gt;3&gt;'></input>";
+    butt = butt + "<input onclick='openPersonForm4(\"" + p.id + "\")' type='button' value='&gt;4&gt;'></input>";
+    butt = butt + "<input onclick='openPersonForm5(\"" + p.id + "\")' type='button' value='&gt;5&gt;'></input>";
+    
+    $('#theList tr:last').after("<tr><td>" + p.id_paciente + "</td><td>" + nome + "</td><td>" + getSexoString(p.sexo) + "</td><td>" + getAgeString(p) + "</td><td>" + p.af + "</td><td>" + butt + "</td></tr>");
 }
 
 function openPersonForm(rowId) {
@@ -94,6 +100,26 @@ function openPersonForm(rowId) {
     odkTables.editRowWithSurvey(null, tableId, rowId, formId, screenPath);
 }
 
+
+function openPersonForm2(rowId) {
+    var rowId = rowId;
+    odkTables.editRowWithSurvey(null, 'QPS', rowId, "QPS", "initial");
+}
+
+function openPersonForm3(rowId) {
+    var rowId = rowId;
+    odkTables.editRowWithSurvey(null, 'QPS', rowId, "inscricao", "initial");
+}
+
+function openPersonForm4(rowId) {
+    var rowId = rowId;
+    odkTables.editRowWithSurvey(null, 'QPS', rowId, "QPS", null, null);
+}
+
+function openPersonForm5(rowId) {
+    var rowId = rowId;
+    odkTables.editRowWithSurvey(null, 'QPS', rowId, "inscricao", null, null);
+}
 
 /* adate helper */
 function getAdateHelper() {return {
