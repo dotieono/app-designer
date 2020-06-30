f378e4d67ec146cbb0bb8a75059eb93a1654d4ba
Emil Rossing <emil@rossing.org>
2020-06-30 16:31:23 +0200

    Added sc.getMonths(year,months)

--- app/system/survey/js/sapiensConsultHelpers.js
+++ app/system/survey/js/sapiensConsultHelpers.js
@@ -4,6 +4,9 @@ function(moment) {
         isGbPhone: function(str) {
             var regex = /^9[5|6]\d{7}$/gm;
             return (str.toString(10).match(regex)) ? true : false;
+        },
+        getMonths: function(year, months) {
+            return 12 * parseInt(year,10) + parseInt(months,10)
         }
     }
 });
