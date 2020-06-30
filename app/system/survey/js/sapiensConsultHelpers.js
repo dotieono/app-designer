1e7fbeb7dc85e472bcc58871cfda3fbeab7d9dca
Celso <celso@sapiensconsult.com>
2020-06-30 15:27:13 +0200

    Fixe

--- app/system/survey/js/sapiensConsultHelpers.js
+++ app/system/survey/js/sapiensConsultHelpers.js
@@ -2,6 +2,6 @@ define(['moment'],
 function(moment) {
     isGbPhone: function(str) {
         var regex = /^9[5|6]\d{7}$/gm;
-        return (str.match(regex)) ? true : false;
+        return (str.toString(10).match(regex)) ? true : false;
     }
 });
